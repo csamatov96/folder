5c049112b217fe7ddde03b73e7cf1ebadf2f9b1d
csamatov96 <csamatov96@gmail.com>
2019-12-21 11:29:36 -0600

    changes

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" { #
-#  count = 3
+  count = 10
   ami             = "ami-00068cd7555f543d5"
   instance_type   = "t2.micro"
   associate_public_ip_address = "true"
@@ -18,6 +18,7 @@ resource "aws_instance" "web" { #
 #resource "aws_instance" "manually" {
 #    ami = "ami-00068cd7555f543d5"
 #    key_name = aws_key_pair.key_resource.key_name
-#    security_groups = ["allow_ssh_http"]  
+#    security_groups = ["allow_ssh_http"] 
+#    instance_type = "t2.micro"
 # }
 }
